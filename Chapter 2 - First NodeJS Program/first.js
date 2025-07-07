56e713fe8bca56b689d527cc5a7160f67ba9b896
sonu962324 <sonu96om@gmail.com>
2025-07-07 22:40:00 +0530

    Delete Chapter 2 - First NodeJS Program directory

--- Chapter 2 - First NodeJS Program/first.js
+++
@@ -1,7 +0,0 @@
-console.log("KG Coding is the best.");
-
-const fs = require('fs');
-fs.writeFile("output.txt", "Writing File", (err) => {
-  if (err) console.log("Error occurred");
-  else console.log('File Written Successfully');
-});
